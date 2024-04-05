badb7fd12c47d81e677f8bc89769643d388900d8
Peilun Jiang <peilunjuice@gmail.com>
2024-04-05 15:08:06 -0400

    initial commit

---
+++ MonsterDiagnosisAgent/MonsterDiagnosisAgent.py
@@ -0,0 +1,27 @@
+class MonsterDiagnosisAgent:
+    def __init__(self):
+        # If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, diseases, patient):
+        # Add your code here!
+        #
+        # The first parameter to this method is a list of diseases, represented as a
+        # dictionary. The key in each dictionary entry is the name of a disease. The
+        # value for the key item is another dictionary of symptoms of that disease, where
+        # the keys are letters representing vitamin names ("A" through "Z") and the values
+        # are "+" (for elevated), "-" (for reduced), or "0" (for normal).
+        #
+        # The second parameter to this method is a particular patient's symptoms, again
+        # represented as a dictionary where the keys are letters and the values are
+        # "+", "-", or "0".
+        #
+        # This method should return a list of names of diseases that together explain the
+        # observed symptoms. If multiple lists of diseases can explain the symptoms, you
+        # should return the smallest list. If multiple smallest lists are possible, you
+        # may return any sufficiently explanatory list.
+        #
+        # The solve method will be called multiple times, each of which will have a new set
+        # of diseases and a new patient to diagnose.
+        pass
+
